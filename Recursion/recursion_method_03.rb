e35475d5e60b47dbae0b0881257a48f97a67c2c3
shinchan12345678 <102851643+shinchan12345678@users.noreply.github.com>
2022-08-14 15:38:24 +0900

    practice

--- Recursion/recursion_method_03.rb
+++ Recursion/recursion_method_03.rb
@@ -2,44 +2,50 @@
 
 def fibo(n)
   # puts "fibo#{n}を呼び出しました"
-  if n == 0
-    return 0
-  elsif  n == 1
+  if n == 0 # 第1項目の場合、1を返す
+    return 1
+  elsif  n == 1 # 第2項目の場合、1を返す
     return 1
   end
-  result = fibo(n-1) + fibo(n-2)
+
+  # 第3項以降の場合、n-1項とn-2項の値を足した値を返す
+  result = fibo(n-1) + fibo(n-2) # 関数fibo()の中で、関数fibo()を呼び出している
 
   # puts "#{n}項目 = #{result}"
   return result
 end
 
-puts fibo(10)
-
-# # forループ
-i = 2
-f = [1,1]
-while i < 10 do
-  f.push(f[i-1]+f[i-2])
-  # puts "#{i}項目：#{f[i]}"
-  i += 1
+11.times do |i|
+  puts "#{i+1}項目" + fibo(i).to_s
 end
 
-puts f.last
+# # # forループ
+# i = 2
+# f = [1,1]
+# while i < 11 do
+#   f.push(f[i-1]+f[i-2])
+#   # puts "#{i}項目：#{f[i]}"
+#   i += 1
+# end
+
+# puts f.last
 
 
 
-@memo = {}
+@memo = {} #計算結果をメモしておく、ハッシュを用意
 def long_fibo(n)
-  if n == 0
-    return 0
-  elsif n == 1
+  if n == 0 # 第1項目の場合、1を返す
+    return 1
+  elsif n == 1 # 第2項目の場合、1を返す
     return 1
   end
-
+  # 第3項以降の場合、メモを確認し既知の値であればメモから値を取り出す。なければ、n-1項とn-2項の値を足した値を返す
   return @memo[n] if @memo[n]
   return @memo[n] = long_fibo(n-1)+long_fibo(n-2)
 end
 
-long_fibo(10)
+11.times do |i|
+  puts "#{i+1}項目" + long_fibo(i).to_s
+end
 
-puts @memo
+puts "メモを確認(keyに1を足した値が項数)：#{@memo}"
