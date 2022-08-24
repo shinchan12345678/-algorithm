9f9e788294145ede6c1925b3a553a98b175c8b54
shinchan12345678 <102851643+shinchan12345678@users.noreply.github.com>
2022-08-24 11:27:46 +0900

    practice

---
+++ practice/DynamicProgramming/code5_8.rb
@@ -0,0 +1,40 @@
+@s = "logistic"
+@t = "algorithm"
+
+s_n = @s.size
+t_n = @t.size
+
+@dp = {}
+(s_n+1).times do |i|
+  @dp[i]={}
+  (t_n+1).times do |k|
+    @dp[i][k] = 10000000
+  end
+end
+
+@dp[0][0] = 0
+
+
+i = 0
+k = 0
+
+while i <= s_n do
+  while k <= t_n do
+    if i > 0 && k > 0
+      if @s[i-1] == @t[k-1]
+        @dp[i][k] = [@dp[i][k],@dp[i-1][k-1]].min
+      else
+        @dp[i][k] = [@dp[i][k],@dp[i-1][k-1]+1].min
+      end
+    end
+
+    @dp[i][k] = [@dp[i][k],@dp[i-1][k]+1].min  if i > 0
+    @dp[i][k] = [@dp[i][k],@dp[i][k-1]+1].min  if k > 0 
+    k += 1 
+  end
+  k = 0
+  i += 1
+end
+
+
+puts @dp
